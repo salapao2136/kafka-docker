062c4c4ea8378241b9ee45fec0751375dddbb618
Thanakorn.K <potook_02@hotmail.com>
2018-07-05 16:11:40 +0700

    first commit

---
+++ consumer-high-level.js
@@ -0,0 +1,13 @@
+const { Client, HighLevelConsumer } = require('kafka-node')
+var client = new Client('localhost:2181')
+var topics = [{ topic: 'test-kafka' }]
+var options = { autoCommit: true, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 }
+var consumer = new HighLevelConsumer(client, topics, options)
+
+consumer.on('message', function (message) {
+  console.log(message)
+})
+
+consumer.on('error', function (err) {
+  console.log('error', err)
+})
